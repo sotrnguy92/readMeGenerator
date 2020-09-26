5c94e47594c6c497067a9fb3a3ffc60d2f949e2d
sotrnguy92 <sonnguyen092@gmail.com>
2020-09-26 15:42:39 -0700

    added inquirer questions and tested the index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const questions = [
     {
         type: 'input',
         name: 'name',
-        message: 'What is your First and Last Name'
+        message: 'What is your First and Last Name?'
     },
     {
         type: 'input',
@@ -14,16 +14,49 @@ const questions = [
     },
     {
         type: 'input',
-        name: 'year',
-        message: 'What is the year?'
+        name: 'project',
+        message: 'What is the title of your project?'
     },
     {
         type: 'input',
-        name: 'year',
-        message: 'What is the year?'
+        name: 'github',
+        message: 'What is your github username?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a short description of your project.'
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'what command should be used to install dependencies? Default is:',
+        default: 'npm i'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How is this repo being used? Default is:',
+        default: 'Run npm i'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'What command should I use to test? Default is:',
+        default: 'npm test'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What license will this project have?',
+        choices: ['MIT','Mozilla Public License 2.0', 'Apache license 2.0', 'Do What The F*ck You Want To Public License', 'Boost Software License 1.0']
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'What does user need to know about contributing? Default is:',
+        default: 'Make a pull request'
     }
-
-
 
 ];
 
@@ -31,7 +64,8 @@ function writeToFile(fileName, data) {
 }
 
 function init() {
-
+    inquirer
+        .prompt(questions)
 }
 
 init();
